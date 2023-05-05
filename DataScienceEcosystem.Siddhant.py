2aff4a9a88588c72c4447559e33c409c12bd9533
ImSiddhaant <129687067+ImSiddhaant@users.noreply.github.com>
2023-05-05 12:54:51 +0200

    Update DataScienceEcosystem.Siddhant.py

--- DataScienceEcosystem.Siddhant.py
+++ DataScienceEcosystem.Siddhant.py
@@ -1,59 +1,58 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-#  # Data Science Tools and Ecosystem
-
-# In this notebook, Data Science Tools and Ecosystem are summarized.
-
-# **Objectives:**
-# 
-# + List popular languages for Data Science
-# + Common used libraries in Data Science
-# + Data Science Tools
-# + Few arithmetic expressions
-# 
-
-# Some of the popular languages that Data Scientists use are:
-# 
-#     1. Python
-#     2. R programming language
-#     3. Scala
-#     4. Java
-#     5. C++
-#     6. Julia 
-
-# Some of the commonly used libraries used by Data Scientists include:
-# 
-#    1. Python - Pandas, NumPy, SciPy, and MatPotlib.
-#    2. R - dplyr, stringr, ggplot, and  caret.
-#    3. Scala - Shark, MLlib, GraphX, and Spark streaming.
-#    4. Java - Java-ML, Apache MLlib, and Deeplearning4.
-#    5. C++ - TensorFlow, MongoDB, and Caffe.
-#    6. Julia - JuliaDB.
-
-# |**Data Science Tools**|
-# |---------------------|
-# |Jupyter Notebooks|
-# |JupyterLab|
-# |Apache Zeppelin|
-# |RStudio|
-# |Spyder|
-
-# ### Below are a few examples of evaluating arithmetic expressions in Python
-
-# In[2]:
 
 
-#This a simple arithmetic expression to mutiply then add integers
+ # Data Science Tools and Ecosystem
+
+ In this notebook, Data Science Tools and Ecosystem are summarized.
+
+ **Objectives:**
+ 
+ + List popular languages for Data Science
+ + Common used libraries in Data Science
+ + Data Science Tools
+ + Few arithmetic expressions
+ 
+
+ Some of the popular languages that Data Scientists use are:
+ 
+     1. Python
+     2. R programming language
+     3. Scala
+     4. Java
+     5. C++
+     6. Julia 
+
+ Some of the commonly used libraries used by Data Scientists include:
+ 
+    1. Python - Pandas, NumPy, SciPy, and MatPotlib.
+    2. R - dplyr, stringr, ggplot, and  caret.
+    3. Scala - Shark, MLlib, GraphX, and Spark streaming.
+    4. Java - Java-ML, Apache MLlib, and Deeplearning4.
+    5. C++ - TensorFlow, MongoDB, and Caffe.
+    6. Julia - JuliaDB.
+
+ |**Data Science Tools**|
+ |---------------------|
+ |Jupyter Notebooks|
+ |JupyterLab|
+ |Apache Zeppelin|
+ |RStudio|
+ |Spyder|
+
+ ### Below are a few examples of evaluating arithmetic expressions in Python
+
+ In[2]:
+
+
+This a simple arithmetic expression to mutiply then add integers
 (3*4)+5 
 
 
-# In[3]:
+ In[3]:
 
 
-#This will convert 200 minutes to hours by diving by 60
+This will convert 200 minutes to hours by diving by 60
 200/60
 
 
-# ## Author
-# Siddhant Atman Padale
+ ## Author
+ Siddhant Atman Padale
